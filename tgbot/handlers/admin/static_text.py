9e4613f1c13fae78335e576d97836e6140d6279f
KeeperT <36564215+KeeperT@users.noreply.github.com>
2022-05-27 16:11:23 +0300

    scenario update v5

--- tgbot/handlers/admin/static_text.py
+++ tgbot/handlers/admin/static_text.py
@@ -22,22 +22,24 @@ negative_answer = df_texts.feedback[3]
 stock_choice = df_texts.stock_choice[0]
 nasdaq100_chosen = df_texts.stock_choice[3]
 sp500_chosen = df_texts.stock_choice[4]
-all_shares_chosen = f'{df_texts.stock_choice[5]} 👍'
+all_shares_chosen = df_texts.stock_choice[5]
 
 time_settings = df_texts.time[0]
-time_settings_unlimited = f'{df_texts.time[1]} 👍'
+time_settings_unlimited = df_texts.time[1]
 
 off_signals = df_texts.off_signals[0]
 
-start_created = (f'На данный момент я умею работать с двумя стратегиями:\n'
+hello = df_texts.hello[1]
+hello_2 = df_texts.hello[2]
+start_created = (f'{hello}\n'
                  f'\n'
                  f'<a href="{sma_strategy_link}"><b>Среднее скользящее (cross-SMA)</b></a>\n'
                  f'<a href="{rsi_strategy_link}"><b>Перепроданность по RSI</b></a>\n'
                  f'\n'
-                 f'Давай выберем стратегию! 😎')
-start_not_created = (f'На данный момент я умею работать с двумя стратегиями:\n'
+                 f'{hello_2}')
+start_not_created = (f'{hello}\n'
                      f'\n'
                      f'<a href="{sma_strategy_link}"><b>Среднее скользящее (cross-SMA)</b></a>\n'
                      f'<a href="{rsi_strategy_link}"><b>Перепроданность по RSI</b></a>\n'
                      f'\n'
-                     f'Давай выберем стратегию! 😎')
+                     f'{hello_2}')
